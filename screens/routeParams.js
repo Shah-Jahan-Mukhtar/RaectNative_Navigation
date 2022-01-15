cf61fe9f963238149642ff1b1f78fa2860f2ac2f
Shah-jahan Mukhtar <Shahjahanmukhtar62@gmail.com>
2022-01-15 15:58:40 +0500

    Navigation

---
+++ screens/routeParams.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View,Text,StyleSheet } from 'react-native'
+
+
+
+const Route_Params=({route})=>{
+    const item=route.params;
+return(
+
+    
+
+);
+};
+
+
+
+export default Route_Params;

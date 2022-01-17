9e24e7464919078ce1e56e3512758c4654709a11
Shah-jahan Mukhtar <Shahjahanmukhtar62@gmail.com>
2022-01-17 13:16:36 +0500

    RouteParams

--- screens/routeParams.js
+++ screens/routeParams.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { View,Text,StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import Colors from "../components/colorBox";
 
-
-
-const Route_Params=({route})=>{
-    const item=route.params;
-return(
-
-    
-
-);
+const Route_Params = ({ route }) => {
+  const { colorName, hexCode, item } = route.params;
+  return (
+    <FlatList
+      data={item}
+      keyExtractor={(item) => item.hexCode}
+      renderItem={({ item }) => (
+        <Colors name={item.colorName} hexCode={item.hexCode} />
+      )}
+      ListHeaderComponent={<Text>{colorName}</Text>}
+    />
+  );
 };
-
-
 
 export default Route_Params;
